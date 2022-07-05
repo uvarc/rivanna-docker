dff58d0c63b25aac8b1a5e47cab30c82d8c6ed40
Alois Duston <alois.duston@parsely.com>
2022-07-05 14:13:29 -0400

    added openexr container

---
+++ deeplabcut/rent_bot.py
@@ -0,0 +1,16 @@
+import os
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+import sys
+import numpy as np
+import pandas as pd
+import regex as re
+import requests
+import lxml
+from lxml.html.soupparser import fromstring
+import prettify
+import numbers
+import htmltext
